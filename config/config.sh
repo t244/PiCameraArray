f93c7f883c64232316e7cc192b9ef2ce28110c6b
Tsuyoshi <takatani@iit.tsukuba.ac.jp>
2026-02-08 01:58:59 +0900

    Deleted start right after config

--- config/config.sh
+++ config/config.sh
@@ -7,4 +7,3 @@ sudo chmod +x /home/pi/PiCameraArray/capture/startup.sh
 sudo cp /home/pi/PiCameraArray/config/picamera-capture.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable picamera-capture.service
-sudo systemctl start picamera-capture.service
