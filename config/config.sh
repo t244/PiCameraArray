9301041a9d5b6db76f343a325b9de788c199bf12
Tsuyoshi <takatani@iit.tsukuba.ac.jp>
2026-02-08 01:31:19 +0900

    Add executable flag

--- config/config.sh
+++ config/config.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Make the startup script executable
+sudo chmod +x /home/pi/PiCameraArray/capture/startup.sh
+
+# Install and start the picamera-capture systemd service
 sudo cp /home/pi/PiCameraArray/config/picamera-capture.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable picamera-capture.service
