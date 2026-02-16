73fad454b777b1c63d914db85ed47d6b8e700191
Tsuyoshi <takatani@iit.tsukuba.ac.jp>
2026-02-17 04:33:43 +0900

    Fix rpicam

--- check_exposure.sh
+++ check_exposure.sh
@@ -7,7 +7,7 @@ echo 0 > /sys/module/imx296/parameters/trigger_mode
 # Exposure time in microseconds (e.g., 10000 = 10ms)
 EXPOSURE_US=${1:?"Usage: $0 <exposure_us> (e.g., $0 10000 for 10ms)"}
 
-libcamera-still -t 0 \
+rpicam-hello -t 0 \
     --viewfinder-width 1456 --viewfinder-height 1088 \
     --shutter $EXPOSURE_US \
     --gain 1.0 \
